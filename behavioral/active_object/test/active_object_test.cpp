911ae2c71e4b291d30af8a94e9fe20d70f245b83
Your Name <Your Email>
2018-02-09 12:57:44 +0900

    chore: Preparing template.

---
+++ behavioral/active_object/test/active_object_test.cpp
@@ -0,0 +1,13 @@
+// Copyright <2018> Tomoyuki-Nakabayashi
+// This software is released under the MIT License, see LICENSE.
+
+#include <gtest/gtest.h>
+
+namespace active_object_test {
+class ActiveObjectTest : public ::testing::Test {
+};
+
+TEST_F(ActiveObjectTest, FirstTest) {
+  EXPECT_TRUE(true);
+}
+}  // namespace active_object_test
